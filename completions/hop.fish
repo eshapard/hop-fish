e74d84a278b6e39ce50e168fc03c0c070cbd09b8
Edward Shapard <ed.shapard@gmail.com>
2022-02-05 10:12:56 -0800

    removed old commented lines in completion file

--- completions/hop.fish
+++ completions/hop.fish
@@ -1,7 +1,5 @@
 # SYNOPSIS
 #   Completions for hop functions.
 
-#complete -c hop -f -a '(command ls ~/.hop)'
-#complete -c rmhop -f -a '(command ls ~/.hop)'
 complete -c hop -f -a '(command ls $HOPPATH)'
 complete -c rmhop -f -a '(command ls $HOPPATH)'
